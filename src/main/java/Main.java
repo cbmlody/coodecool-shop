77617f635a3ebdb73571294c1503ccecb2decf34
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-25 16:19:20 +0200

    Implement tests for database connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import com.codecool.shop.dao.DatabaseConnection;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        DatabaseConnection dbConn = new DatabaseConnection();
+        dbConn.openConnection();
     }
-
-
 }
