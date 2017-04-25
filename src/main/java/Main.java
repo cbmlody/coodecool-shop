0cd20f8dc53f8b32a49dce05ae051023c84b6c6d
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-25 18:24:16 +0200

    Implement new approach to resetDatabase method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 import com.codecool.shop.dao.DatabaseConnection;
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
         DatabaseConnection dbConn = new DatabaseConnection();
         dbConn.openConnection();
+        try {
+            DatabaseConnection.resetDatabase(dbConn.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            dbConn.closeConnection();
+        }
+        dbConn.closeConnection();
     }
 }
