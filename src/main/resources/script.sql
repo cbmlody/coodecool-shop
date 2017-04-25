4446b491f77592f52ec81258bf162043817be07e
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-25 20:25:58 +0200

    Update content

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,3 +1,52 @@
+DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS `suppliers`;
+DROP TABLE IF EXISTS `product_categories`;
+
+
+CREATE TABLE `products` ( id integer PRIMARY KEY AUTOINCREMENT,
+                          name TEXT NOT NULL,
+                          defaultPrice REAL NOT NULL,
+                          currency CHAR(10),
+                          description CHAR(50),
+                          categoryId  INTEGER,
+                          supplierId INTEGER
+);
+
+create table `product_categories` (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(16),
+  department VARCHAR(50),
+  description TEXT
+);
+
+create table `suppliers` (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50),
+  description TEXT
+);
+
+insert into suppliers (name, description) values ('Apotex Corp.', 'cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque');
+insert into suppliers  (name, description) values ('Kramer Novis', 'diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce posuere felis');
+insert into suppliers  (name, description) values ('Purminerals', 'ante vivamus tortor duis mattis egestas metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero');
+insert into suppliers  (name, description) values ('AuroMedics Pharma LLC', 'scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at');
+insert into suppliers  (name, description) values ('MATERNAL SCIENCE, LLC', 'porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec semper');
+insert into suppliers  (name, description) values ('Rite Aid', 'nulla ultrices aliquet maecenas leo odio condimentum id luctus nec molestie sed justo');
+insert into suppliers  (name, description) values ('Smith & Nephew, Inc.', 'posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam');
+insert into suppliers  (name, description) values ('Hospira, Inc', 'felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec');
+insert into suppliers  (name, description) values ('China Ningbo Shangge Cosmetic Technology Corp.', 'felis donec semper sapien a libero nam dui proin leo odio porttitor id consequat in consequat ut');
+insert into suppliers  (name, description) values ('Aloe Care International, LLC', 'odio donec vitae nisi nam ultrices libero non mattis pulvinar nulla pede ullamcorper augue a');
+
+insert into product_categories (name, department, description) values ('Skin Care', 'Health', 'auctor gravida sem praesent id massa id nisl venenatis lacinia aenean sit amet justo morbi ut odio');
+insert into product_categories (name, department, description) values ('Baby', 'Automotive', 'platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit');
+insert into product_categories (name, department, description) values ('Diet&Fitness', 'Electronics', 'urna ut tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non velit nec nisi vulputate');
+insert into product_categories (name, department, description) values ('Pharmacy', 'Clothing', 'at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed');
+insert into product_categories (name, department, description) values ('Household', 'Tools', 'viverra pede ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper');
+insert into product_categories (name, department, description) values ('Baby', 'Toys', 'nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus et magnis');
+insert into product_categories (name, department, description) values ('Personal Health', 'Music', 'ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis est');
+insert into product_categories (name, department, description) values ('Baby', 'Automotive', 'scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec');
+insert into product_categories (name, department, description) values ('Vitamins', 'Games', 'quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum');
+insert into product_categories (name, department, description) values ('Hair Products', 'Books', 'est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis');
+
 insert into products (name, defaultPrice, currency, description, categoryId, supplierId) values ('Dr.Jart CC Essence Balm 02 Medium - Deep', 127.71, 'CNY', 'rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta', 9, 7);
 insert into products (name, defaultPrice, currency, description, categoryId, supplierId) values ('Atenolol', 764.85, 'IDR', 'turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede', 1, 5);
 insert into products (name, defaultPrice, currency, description, categoryId, supplierId) values ('Regular Strength Aspirin EC', 163.49, 'EUR', 'tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at', 3, 1);
