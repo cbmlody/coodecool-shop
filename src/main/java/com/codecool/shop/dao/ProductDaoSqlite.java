c86f69312053249b5afe704c74883a80b4b7be07
michalwilk96 <michalwilk96@gmail.com>
2017-04-26 12:07:59 +0200

    Add ProductDaoSqlite.java file implementing ProductDao

---
+++ src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Product;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDaoSqlite implements ProductDao {
+    
+}
