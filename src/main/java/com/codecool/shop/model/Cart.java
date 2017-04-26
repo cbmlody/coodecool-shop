71eac18ac250fd04d127b3accc2f4f676888a3d3
siematypie <mkkc12@gmail.com>
2017-04-26 14:21:05 +0200

    Add inner class CartItem to Cart

--- src/main/java/com/codecool/shop/model/Cart.java
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -1,4 +1,39 @@
 package com.codecool.shop.model;
 
+
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<CartItem> cartItems = new ArrayList<>();
+
+    //TODO: Not sure if it should be inner
+    private class CartItem {
+        Product product;
+        Integer quantity;
+
+        CartItem(Product product, Integer quantity){
+            this.product = product;
+            this.quantity = quantity;
+        }
+    }
+
+//    private TreeMap<Product, Integer> productMap = new TreeMap<>();
+//
+//    public void add(Product product, Integer quantity){
+//        if (productMap.containsKey(product)){
+//            productMap.put(product, productMap.get(product) + quantity);
+//        } else {
+//            productMap.put(product, quantity);
+//        }
+//    }
+//
+//    public void add(Product product){
+//        add(product, 1);
+//    }
+//
+//    @Override
+//    public Iterator<Product> iterator() {
+//        Set<Product> cartProducts= productMap.keySet();
+//        return cartProducts.iterator();
+//    }
 }
