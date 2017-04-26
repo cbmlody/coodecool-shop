9a388a55084f8af21b90176197ddbdb80345f185
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-26 11:57:06 +0200

    Implement ProductCategorySqlite interface

---
+++ src/main/java/com/codecool/shop/dao/ProductCategorySqlite.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.ProductCategory;
+
+
+import java.util.List;
+
+public class ProductCategorySqlite implements ProductCategoryDao{
+
+    @Override
+    public void add(ProductCategory category) {}
+
+    @Override
+    public void remove(int id) {}
+
+    @Override
+    public ProductCategory find(int id) { return  null; }
+
+    @Override
+    public List<ProductCategory> getAll() {
+
+    }
+}
