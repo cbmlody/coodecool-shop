c3f992ee38ffe8b981269165bf621fd87e1b5f75
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-04-26 12:00:18 +0200

    Initial commit

---
+++ src/main/java/com/codecool/shop/dao/SupplierDaoSqlite.java
@@ -0,0 +1,11 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Supplier;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class SupplierDaoSqlite implements SupplierDao {
+
+}
