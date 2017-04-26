a98aea42f6c74944926c7ab03c133af02f81b064
michalwilk96 <michalwilk96@gmail.com>
2017-04-26 12:25:53 +0200

    Implement getAll() method

--- src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
+++ src/main/java/com/codecool/shop/dao/ProductDaoSqlite.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,4 +17,30 @@ public class ProductDaoSqlite implements ProductDao {
 
     @Override
     public void remove(int id) {}
+
+    @Override
+    public List<Product> getAll() {
+        List<Product> productList = new ArrayList<>();
+        ProductCategoryDaoSqlite productCategoryDaoSqlite = new ProductCategoryDaoSqlite();
+        SupplierDaoSqlite supplierDaoSqlite = new SupplierDaoSqlite();
+        Statement statement = DatabaseConnection.getInstance().getStatement();
+        String query = "SELECT *FROM `products`";
+        try {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Product product = new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaulPrice"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        productCategoryDaoSqlite.find(resultSet.getInt("categoryId")),
+                       supplierDaoSqlite.find(resultSet.getInt("supplierId")));
+                productList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
 }
