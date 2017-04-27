886ec92d8295ff98342066936f742bd51f46b13d
siematypie <mkkc12@gmail.com>
2017-04-27 21:58:34 +0200

    Refactor class from static to use instanced

--- src/main/java/com/codecool/shop/controller/MenuController.java
+++ src/main/java/com/codecool/shop/controller/MenuController.java
@@ -16,17 +16,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuController {
-    private static final MenuController INSTANCE = new MenuController();
-    private MenuController() {}
-    private static Scanner read = new Scanner(System.in);
-    private static Boolean quitProgram = true;
-    private static Boolean quitProductMenu = true;
+    private static Cart cart = new Cart();
+    private Scanner read = new Scanner(System.in);
+    private ProductCategoryView categoryView = new ProductCategoryView();
+    private ProductCategoryDaoSqlite categoryDao = new ProductCategoryDaoSqlite();
+    private SupplierDaoSqlite supplierDao = new SupplierDaoSqlite();
+    private SupplierView supplierView = new SupplierView();
+    private ProductDaoSqlite productDao = new ProductDaoSqlite();
+    private CartView cartView = new CartView();
+
     private static Boolean quitCartMenu = true;
     private static Boolean quitSearchMenu = true;
 
-    private static MenuController getInstance() {
-        return INSTANCE;
-    }
 
     public static Integer getUserChoice() {
         System.out.print("Choice: ");
