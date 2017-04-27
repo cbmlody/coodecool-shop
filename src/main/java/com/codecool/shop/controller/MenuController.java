a8847f011105e04dee87795698f6b58fa80f672e
michalwilk96 <michalwilk96@gmail.com>
2017-04-27 14:55:57 +0200

    Implement method to return controller instance

--- src/main/java/com/codecool/shop/controller/MenuController.java
+++ src/main/java/com/codecool/shop/controller/MenuController.java
@@ -1,5 +1,17 @@
 package com.codecool.shop.controller;
 
+import java.util.Scanner;
+
 public class MenuController {
-    
+    private static final MenuController INSTANCE = new MenuController();
+    private MenuController() {}
+    private static Scanner read = new Scanner(System.in);
+    private static Boolean quitProgram = true;
+    private static Boolean quitProductMenu = true;
+    private static Boolean quitCartMenu = true;
+    private static Boolean quitSearchMenu = true;
+
+    private static MenuController getInstance() {
+        return INSTANCE;
+    }g
 }
