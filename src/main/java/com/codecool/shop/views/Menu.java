54dabd7fdb301dc0699ba0df971d7ca3d7390c7f
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-27 10:11:43 +0200

    Add displayMain method

--- src/main/java/com/codecool/shop/views/Menu.java
+++ src/main/java/com/codecool/shop/views/Menu.java
@@ -6,4 +6,13 @@ public final class Menu {
     public static Menu getInstance() {
         return MENU;
     }
+
+    public static void displayMain() {
+        System.out.println("MAIN MENU\n" +
+                "  1. Show products\n" +
+                "  2. Show Cart\n" +
+                "  3. Search\n" +
+                "  0. Exit\n");
+    }
+
 }
