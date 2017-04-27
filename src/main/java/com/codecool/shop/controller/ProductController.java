390101bbfaa799f332b1523942155cb76438cfe4
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-04-27 10:26:10 +0200

    Create return instance

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.codecool.shop.controller;
 
 public class ProductController {
+    private static final ProductController INSTANCE = new ProductController();
+    private  ProductController(){}
+    public static ProductCategoryController getInstance() {
+        return INSTANCE;
+    }
 
 }
