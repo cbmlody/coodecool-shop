de60e27f99c335cbccd2a5fc049e67edb2423ae1
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-04-27 10:39:35 +0200

    Create getAll method

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,5 +1,11 @@
 package com.codecool.shop.controller;
 
+import java.util.List;
+import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.views.ProductView;
+
 public class ProductController {
     private static final ProductController INSTANCE = new ProductController();
     private  ProductController(){}
@@ -7,4 +13,10 @@ public class ProductController {
         return INSTANCE;
     }
 
+    public void getAllProducts(){
+        ProductView productView = new ProductView();
+        ProductDaoSqlite productDaoSqlite = new ProductDaoSqlite();
+        List<Product> productList = productDaoSqlite.getAll();
+        productView.displayAll(productList);
+    }
 }
