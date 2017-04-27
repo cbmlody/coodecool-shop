42379148968e74061d135dae8aa4420ccfa76c87
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-04-27 14:20:52 +0200

    Create payment constructor

--- src/main/java/com/codecool/shop/model/Payment.java
+++ src/main/java/com/codecool/shop/model/Payment.java
@@ -4,5 +4,16 @@ import java.util.Currency;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Payment {
+    private String name;
+    private Integer cardnumber;
+    private Integer price;
+    private Currency currency;
 
+    public Payment(String name, Integer cardnumber, Integer price, Currency currency) {
+        this.name = name;
+        this.cardnumber = cardnumber;
+        this.price = price;
+        this.currency = currency;
+    }
 }
+
