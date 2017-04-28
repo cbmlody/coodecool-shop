94d5f831ae699467e93247be66cbe5694f503ad2
Cezary Broś <cezary.bros+git@gmail.com>
2017-04-28 09:28:18 +0200

    Refactor code, Rename isValidate to isValid

--- src/main/java/com/codecool/shop/model/Payment.java
+++ src/main/java/com/codecool/shop/model/Payment.java
@@ -12,12 +12,12 @@ public class Payment {
         this.cardNumber = cardNumber;
         this.validate = validatePayment();
     }
-    private boolean validatePayment(){
+    private boolean validatePayment() {
         int randomNum = ThreadLocalRandom.current().nextInt(0, 101);
         return randomNum < 70;
     }
 
-    public boolean isValidate() {
+    public boolean isValid() {
         return validate;
     }
 }
