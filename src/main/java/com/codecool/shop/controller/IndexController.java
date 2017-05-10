38ba75e2b9e0912f3bf216dcf93341742f097fc4
michalwilk96 <michalwilk96@gmail.com>
2017-05-10 23:23:43 +0200

    Remove itemsCount variable and params put

--- src/main/java/com/codecool/shop/controller/IndexController.java
+++ src/main/java/com/codecool/shop/controller/IndexController.java
@@ -14,12 +14,9 @@ import java.util.Map;
 public class IndexController {
     public static ModelAndView getAllInfo(Request req, Response res) throws SQLException{
         Map params = new HashMap<>();
-        Integer productsCount = new ProductDaoSqlite().getBy(new ProductCategoryDaoSqlite().find(1)).size();
-        System.out.println(productsCount);
         params.put("productList", new ProductDaoSqlite().getAll());
         params.put("productCategoryList", new ProductCategoryDaoSqlite().getAll());
         params.put("productSupplierList", new SupplierDaoSqlite().getAll());
-        params.put("categoryCounter", new )
         return new ModelAndView(params, "product/index");
     }
 }
