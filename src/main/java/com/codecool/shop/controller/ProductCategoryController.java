7728d6d92b270dc8a43c6424ec5ef4ffb4c6b90d
michalwilk96 <michalwilk96@gmail.com>
2017-05-10 14:19:25 +0200

    Remove productCategoryController() singleton

--- src/main/java/com/codecool/shop/controller/ProductCategoryController.java
+++ src/main/java/com/codecool/shop/controller/ProductCategoryController.java
@@ -8,19 +8,11 @@ import java.util.List;
 
 public final class ProductCategoryController {
 
-    private static final ProductCategoryController INSTANCE = new ProductCategoryController();
-
-    private ProductCategoryController() {
-    }
-
-    public static ProductCategoryController getInstance() {
-        return INSTANCE;
-    }
-
     public void getAllProductCategories() throws SQLException {
         ProductCategoryDaoSqlite productCategoryDaoSqlite = new ProductCategoryDaoSqlite();
         List<ProductCategory> productCategoriesList = productCategoryDaoSqlite.getAll();
     }
+    
     public void findCategory(int id) throws SQLException {
         ProductCategoryDaoSqlite productCategoryDaoSqlite = new ProductCategoryDaoSqlite();
         ProductCategory productCategory = productCategoryDaoSqlite.find(id);
