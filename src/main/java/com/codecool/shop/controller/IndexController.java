dd2ba02963d91ced74aeb89f5514ecefa2315bad
Cezary Broś <cezary.bros+git@gmail.com>
2017-05-10 22:00:25 +0200

    Implement index controller

--- src/main/java/com/codecool/shop/controller/IndexController.java
+++ src/main/java/com/codecool/shop/controller/IndexController.java
@@ -1,4 +1,25 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductCategoryDaoSqlite;
+import com.codecool.shop.dao.ProductDaoSqlite;
+import com.codecool.shop.dao.SupplierDaoSqlite;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class IndexController {
+    public static ModelAndView getAllInfo(Request req, Response res) throws SQLException{
+        Map params = new HashMap<>();
+        Integer productsCount = new ProductDaoSqlite().getBy(new ProductCategoryDaoSqlite().find(1)).size();
+        System.out.println(productsCount);
+        params.put("productList", new ProductDaoSqlite().getAll());
+        params.put("productCategoryList", new ProductCategoryDaoSqlite().getAll());
+        params.put("productSupplierList", new SupplierDaoSqlite().getAll());
+        params.put("categoryCounter", new )
+        return new ModelAndView(params, "product/index");
+    }
 }
