1b6493ae665e855b1af15a740961be1e78972047
michalwilk96 <michalwilk96@gmail.com>
2017-05-10 14:17:07 +0200

    Add getAllSuppliers() method

--- src/main/java/com/codecool/shop/controller/SupplierController.java
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -1,17 +1,25 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.dao.SupplierDaoSqlite;
 import com.codecool.shop.model.Supplier;
-
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class SupplierController {
 
-    public void getAllSuppliers() throws SQLException {
+    public static ModelAndView getAllSuppliers(Request req, Response res) throws SQLException {
         SupplierDaoSqlite supplierDaoSqlite = new SupplierDaoSqlite();
-        List<Supplier> supplierList = supplierDaoSqlite.getAll();
+        Map params = new HashMap<>();
+        params.put("suppliersList", supplierDaoSqlite.getAll());
+        return new ModelAndView(params, "product/index");
     }
+
     public void findSupplier(int id) throws SQLException {
         SupplierDaoSqlite supplierDaoSqlite = new SupplierDaoSqlite();
         Supplier supplier = supplierDaoSqlite.find(id);
