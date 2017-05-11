39efcfeec973daf89892fba9aeabd7423fb8d303
michalwilk96 <michalwilk96@gmail.com>
2017-05-11 10:19:40 +0200

    Add getProductsBySearch()

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,16 +3,11 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductCategoryDaoSqlite;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.dao.SupplierDaoSqlite;
-import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
-
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ProductController {
@@ -27,9 +22,17 @@ public class ProductController {
     public static ModelAndView getProductsBySearch(Request req, Response res) throws SQLException {
         ProductDaoSqlite productDaoSqlite = new ProductDaoSqlite();
         Map params = new HashMap<>();
-        String productSearchName = req.queryParams("search");
-        params.put("productsFoundBySearch", productDaoSqlite.getBy(productSearchName));
-        return new ModelAndView(params, "event/index");
+        String productSearch = req.queryParams("search");
+        if (productSearch.length() >= 3 && !productDaoSqlite.getBy(productSearch).isEmpty()) {
+            params.put("productList", productDaoSqlite.getBy(productSearch));
+        } else if (productSearch.length() == 0) {
+            params.put("productList", productDaoSqlite.getAll());
+        } else {
+            params.put("info", "Products not found...");
+        }
+        params.put("productCategoryList", new ProductCategoryDaoSqlite().getAll());
+        params.put("productSupplierList", new SupplierDaoSqlite().getAll());
+        return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView getBySupplier(Request req, Response res) throws SQLException {
