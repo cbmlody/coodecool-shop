1ae43bdd8f620e1b64f1c7eeb6f0d37c36ab8174
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-05-11 21:36:33 +0200

    Created basket based on session

--- src/main/java/com/codecool/shop/controller/IndexController.java
+++ src/main/java/com/codecool/shop/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductCategoryDaoSqlite;
 import com.codecool.shop.dao.ProductDaoSqlite;
 import com.codecool.shop.dao.SupplierDaoSqlite;
+import com.codecool.shop.model.Cart;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -14,6 +15,20 @@ import java.util.Map;
 public class IndexController {
     public static ModelAndView getAllInfo(Request req, Response res) throws SQLException{
         Map params = new HashMap<>();
+        Cart cart = req.session().attribute("basket");
+        Float sumOfBasket;
+        Integer countItems;
+        if(cart == null){
+            countItems = 0;
+            sumOfBasket = 0f;
+        }
+        else{
+            countItems = cart.size();
+            sumOfBasket = cart.getSum();
+        }
+        System.out.println(countItems);
+        params.put("count",countItems);
+        params.put("sum",sumOfBasket);
         params.put("productList", new ProductDaoSqlite().getAll());
         params.put("productCategoryList", new ProductCategoryDaoSqlite().getAll());
         params.put("productSupplierList", new SupplierDaoSqlite().getAll());
