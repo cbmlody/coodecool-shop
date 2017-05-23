24039781500128312b90b4924df2677145c1b3c2
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-24 01:12:24 +0200

    Add tests for setters & getters (Price, ConvertedFloatPrice, BaseCurrency, ProductCategory,Supplier), equals, toString, change BaseCurrency methods

--- src/tests/com/codecool/shop/model/ProductTest.java
+++ src/tests/com/codecool/shop/model/ProductTest.java
@@ -1,7 +1,12 @@
 package com.codecool.shop.model;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 
 /**
@@ -10,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ProductTest {
 
 	@Test
-	public void testProductConstructorWithoutIdProductCategorySupplier(){
+	public void testProductConstructorWithoutIdProductCategoryAndSupplier(){
 		Product product = new Product("testName", 100f, "PLN", "testDesc", null, null);
 		assertEquals(Product.class, product.getClass());
 	}
@@ -47,6 +52,115 @@ class ProductTest {
 		Product product = generateProductWithId();
 		assertEquals("100.0 PLN", product.getPrice());
 	}
+
+	@Test
+	public void testSetPrice(){
+		Product product = generateProductWithId();
+		product.setPrice(200f, "PLN");
+		assertEquals("200.0 PLN", product.getPrice());
+	}
+
+	@Test
+	public void testGetConvertedFloatPrice(){
+		Product product = generateProductWithId();
+		assertEquals(100f,product.getConvertedFloatPrice());
+	}
+
+	@Test
+	public void testGetBaseCurrencyReturnedClass(){
+		Product product = generateProductWithId();
+		assertEquals(Currency.class, product.getDefaultCurrency().getClass());
+	}
+
+	@Test
+	public void testGetBaseCurrencyReturnedValue(){
+		Product product = generateProductWithId();
+		assertEquals(Currency.getInstance("PLN"),product.getDefaultCurrency());
+	}
+
+	@Test
+	public void testChangeBaseCurrency(){
+		Product product = generateProductWithId();
+		product.setDefaultCurrency(Currency.getInstance("PHP"));
+		assertEquals(Currency.getInstance("PHP"), product.getDefaultCurrency());
+	}
+
+	@Test
+	public void testGetProductCategoryNotNull(){
+		Product product = generateProductWithId();
+		assertNotNull(product.getProductCategory());
+	}
+
+	@Test
+	public void testGetProductCategoryReturnClass(){
+		Product product = generateProductWithId();
+		assertEquals(ProductCategory.class, product.getProductCategory().getClass());
+	}
+
+	@Test
+	public void testSetProductCategory(){
+		Product product = generateProductWithId();
+		ProductCategory productCategory = new ProductCategory("testSetter", "testDepartment", "testDescription");
+		product.setProductCategory(productCategory);
+		assertEquals(productCategory.getName(), product.getProductCategory().getName());
+	}
+
+	@Test
+	public void testGetSupplierReturnClass(){
+		Product product = generateProductWithId();
+		assertEquals(Supplier.class, product.getSupplier().getClass());
+	}
+
+	@Test
+	public void testGetSupplierNotNull() {
+		Product product = generateProductWithId();
+		assertNotNull(product.getSupplier());
+	}
+
+	@Test
+	public void testSetSupplier() {
+		Product product = generateProductWithId();
+		Supplier supplier = new Supplier("testSetSupplier", "testDescription");
+		product.setSupplier(supplier);
+		assertEquals(supplier.getName(), product.getSupplier().getName());
+	}
+
+	@Test
+	public void testEqualsObjNull(){
+		Product product = generateProductWithId();
+		Product product2 = null;
+		assertEquals(false, product.equals(product2));
+	}
+
+	@Test
+	public  void testEqualsObjDiffClass(){
+		Product product = generateProductWithId();
+		Supplier supplier = new Supplier("testSetSupplier", "testDescription");
+		assertEquals(false, product.equals(supplier));
+	}
+
+	@Test
+	public  void testEqualsObjSameClassDiffValues(){
+		Product product1 = generateProductWithId();
+		Supplier supplier = new Supplier("testEqualsProduct","testEqualsDescription");
+		ProductCategory productCategory = new ProductCategory("testEqualsProduct", "testEqualsDept", "testEqualsDescription");
+		Product product2 = new Product(1,"testEquals", 200f, "PHP", "testEqualsDesc", productCategory, supplier);
+		assertEquals(false, product1.equals(product2));
+	}
+
+	@Test
+	public void testEqualsObjSameClassSameObj(){
+		Product product = generateProductWithId();
+		assertEquals(true, product.equals(generateProductWithId()));
+	}
+
+	@Test
+	public void testToString(){
+		Product product = generateProductWithId();
+		assertEquals("id: 1, name: testName, defaultPrice: 100.000000, defaultCurrency: PLN, " +
+		 "productCategory: testName, supplier: testName", product.toString());
+	}
+
 
 	private Product generateProductWithId(){
 		Supplier supplier = new Supplier("testName","testDescription");
