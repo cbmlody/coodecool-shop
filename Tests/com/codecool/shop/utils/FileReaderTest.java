8ea08ffc34c56d3bf84faaf30168956b5d55ecfb
Łukasz <l.bielenin@gmail.com>
2017-05-24 17:12:53 +0200

    Implement test for checking for IOexcpetion

--- Tests/com/codecool/shop/utils/FileReaderTest.java
+++ Tests/com/codecool/shop/utils/FileReaderTest.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +18,12 @@ class FileReaderTest {
     void setUp() {
         testReader = new FileReader();
     }
+
+    @Test
+    void testIfGetStringFromFileThrowsException() {
+        assertThrows(IOException.class, () ->
+        testReader.getStringFromFile("/iuhqgabfiseuabf.txt"));
+    }
+
 
 }
