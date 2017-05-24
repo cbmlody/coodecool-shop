a9b075b464710b35c289a1289df1f5f4b4f0bc5d
Łukasz <l.bielenin@gmail.com>
2017-05-24 16:54:58 +0200

    Implement setUp method in FileReader test

--- Tests/com/codecool/shop/utils/FileReaderTest.java
+++ Tests/com/codecool/shop/utils/FileReaderTest.java
@@ -1,10 +1,19 @@
 package com.codecool.shop.utils;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by mercutio on 24.05.17.
  */
 class FileReaderTest {
+
+    FileReader testReader;
+
+    @BeforeEach
+    void setUp() {
+        testReader = new FileReader();
+    }
 
 }
