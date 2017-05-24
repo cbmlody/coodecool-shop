3de0afd814469fda104b827675e0ec01f3292cce
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-24 22:56:20 +0200

    Add testChangeBaseCurrency, testGetBaseCurrency, tesSetMinusPrice, testSetMinusDefaultPrice

--- src/tests/com/codecool/shop/model/ProductTest.java
+++ src/tests/com/codecool/shop/model/ProductTest.java
@@ -1,10 +1,15 @@
 package com.codecool.shop.model;
 
+import com.codecool.shop.dao.ProductDaoSqlite;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
 import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -17,6 +22,14 @@ class ProductTest {
 	@Test
 	public void testProductConstructorWithoutIdProductCategoryAndSupplier(){
 		Product product = new Product("testName", 100f, "PLN", "testDesc", null, null);
+		assertEquals(Product.class, product.getClass());
+	}
+
+	@Test
+	public void testProductConstructorWithIncorrectCurrencyCode(){
+		Supplier supplier = new Supplier("testName","testDescription");
+		ProductCategory productCategory = new ProductCategory("testName", "testDepartment", "testDescription");
+		Product product = new Product(1,"testName", 100f, "ABC", "testDesc", productCategory, supplier);
 		assertEquals(Product.class, product.getClass());
 	}
 
@@ -48,6 +61,15 @@ class ProductTest {
 	}
 
 	@Test
+	public void testSetMinusDefaultPrice(){
+		Product product = generateProductWithId();
+		product.setDefaultPrice(-20f);
+		assertThrows(IllegalArgumentException.class, () ->{
+			product.getDefaultPrice();
+		});
+	}
+
+	@Test
 	public void testGetPrice(){
 		Product product = generateProductWithId();
 		assertEquals("100.0 PLN", product.getPrice());
@@ -61,28 +83,50 @@ class ProductTest {
 	}
 
 	@Test
+	public void testSetMinusPrice(){
+		Product product = generateProductWithId();
+		assertThrows(IllegalArgumentException.class, () -> {
+			 product.setPrice(-200f, "PLN");
+		});
+	}
+
+	@Test
 	public void testGetConvertedFloatPrice(){
 		Product product = generateProductWithId();
 		assertEquals(100f,product.getConvertedFloatPrice());
 	}
 
 	@Test
-	public void testGetBaseCurrencyReturnedClass(){
+	public void testGetDefaultCurrencyReturnedClass(){
 		Product product = generateProductWithId();
 		assertEquals(Currency.class, product.getDefaultCurrency().getClass());
 	}
 
 	@Test
-	public void testGetBaseCurrencyReturnedValue(){
+	public void testGetDefaultCurrencyReturnedValue(){
 		Product product = generateProductWithId();
 		assertEquals(Currency.getInstance("PLN"),product.getDefaultCurrency());
 	}
 
 	@Test
-	public void testChangeBaseCurrency(){
+	public void testChangeDefaultCurrency(){
 		Product product = generateProductWithId();
 		product.setDefaultCurrency(Currency.getInstance("PHP"));
 		assertEquals(Currency.getInstance("PHP"), product.getDefaultCurrency());
+	}
+
+	@Test
+	public void testGetBaseCurrencyReturnedValue(){
+		Product.changeBaseCurrency(Currency.getInstance("PHP"));
+		assertEquals(Currency.getInstance("PHP"), Product.getBaseCurrency());
+	}
+
+	@Test
+	public void testChangeBaseCurrency(){
+		Currency beforeChangeCurrency = Product.getBaseCurrency();
+		Product.changeBaseCurrency(Currency.getInstance("USD"));
+		Currency afterChangeCurrency = Product.getBaseCurrency();
+		assertNotEquals(beforeChangeCurrency,afterChangeCurrency);
 	}
 
 	@Test
@@ -144,7 +188,7 @@ class ProductTest {
 		Product product1 = generateProductWithId();
 		Supplier supplier = new Supplier("testEqualsProduct","testEqualsDescription");
 		ProductCategory productCategory = new ProductCategory("testEqualsProduct", "testEqualsDept", "testEqualsDescription");
-		Product product2 = new Product(1,"testEquals", 200f, "PHP", "testEqualsDesc", productCategory, supplier);
+		Product product2 = new Product(2,"testEquals", 200f, "PHP", "testEqualsDesc", productCategory, supplier);
 		assertEquals(false, product1.equals(product2));
 	}
 
