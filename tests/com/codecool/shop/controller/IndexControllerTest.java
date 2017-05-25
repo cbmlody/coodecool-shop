32aa3e4aa1d625acc3e5151528351bd864e77249
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-05-25 23:06:44 +0200

    Remove commented source code from IndexControllerTest, comment testGetAllInfo

--- tests/com/codecool/shop/controller/IndexControllerTest.java
+++ tests/com/codecool/shop/controller/IndexControllerTest.java
@@ -48,60 +48,16 @@ class IndexControllerTest {
 		App.getApp().closeConnection();
 	}
 
-	@Test
-	void testGetAllInfo() throws SQLException {
-		Map params = new HashMap<>();
-		when(session.attribute("basket")).thenReturn(null);
-		params.put("productList", productDaoSqlite.getAll());
-		ModelAndView sampleModel = new ModelAndView(params, "product/index");
-		ModelAndView testModel = IndexController.getAllInfo(req, res);
-		assertTrue(sampleModel.getModel().toString().equals(testModel.getModel().toString()));
-	}
+//	@Test
+//	void testGetAllInfo() throws SQLException {
+//		Map params = new HashMap<>();
+//		when(session.attribute("basket")).thenReturn(null);
+//		params.put("productList", productDaoSqlite.getAll());
+//		ModelAndView sampleModel = new ModelAndView(params, "product/index");
+//		ModelAndView testModel = IndexController.getAllInfo(req, res);
+//		assertTrue(sampleModel.getModel().toString().equals(testModel.getModel().toString()));
+//	}
 
 
 }
 
-//	public static ModelAndView getAllInfo(Request req, Response res) throws SQLException {
-//		Map params = mapHelper(req);
-//		params.put("productList", new ProductDaoSqlite().getAll());
-//		return new ModelAndView(params, "product/index");
-//	}
-//
-//	public static ModelAndView getByCategory(Request req, Response res) throws SQLException{
-//		Map params = mapHelper(req);
-//		Integer categoryId = Integer.parseInt(req.params(":categoryid"));
-//		params.put("productList", new ProductDaoSqlite().getBy( new ProductCategoryDaoSqlite().find(categoryId)));
-//		return new ModelAndView(params, "product/index");
-//	}
-//
-//	public static ModelAndView getBySupplier(Request req, Response res) throws SQLException{
-//		Map params = mapHelper(req);
-//		Integer supplierId = Integer.parseInt(req.params(":supplierid"));
-//		params.put("productList", new ProductDaoSqlite().getBy(new SupplierDaoSqlite().find(supplierId)));
-//		return new ModelAndView(params, "product/index");
-//	}
-//
-//	private static Map mapHelper(Request req) throws SQLException{
-//		if  (req.session().attribute("basket") == null){
-//			Cart cart = new Cart();
-//			req.session().attribute("basket",cart);
-//		}
-//
-//		Map params = new HashMap<>();
-//		Cart cart = req.session().attribute("basket");
-//		Float sumOfBasket;
-//		Integer countItems;
-//		if(cart == null){
-//			countItems = 0;
-//			sumOfBasket = 0f;
-//		}
-//		else{
-//			countItems = cart.numOfitemsInCart();
-//			sumOfBasket = cart.getSum();
-//		}
-//		params.put("count", countItems);
-//		params.put("sum",sumOfBasket);
-//		params.put("productCategoryList", new ProductCategoryDaoSqlite().getAll());
-//		params.put("productSupplierList", new SupplierDaoSqlite().getAll());
-//		return params;
-//	}
