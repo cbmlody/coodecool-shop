4cbabd0e1532b79f0bc2cf5df2fdc5e57bfc9b7e
Łukasz <l.bielenin@gmail.com>
2017-05-25 15:10:02 +0200

    Implement Test if getAllSuppliers takes parameters from SQLITE

--- tests/com/codecool/shop/controller/SupplierControllerTest.java
+++ tests/com/codecool/shop/controller/SupplierControllerTest.java
@@ -5,10 +5,16 @@ import com.codecool.shop.dao.ProductCategoryDaoSqlite;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.runner.Request;
+import spark.ModelAndView;
+import spark.Response;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by mercutio on 25.05.17.
@@ -18,9 +24,9 @@ class SupplierControllerTest {
 
     @BeforeEach
     void setUp() throws SQLException {
-//        App.run();
-//        App.getApp().setConnection("jdbc:sqlite:tests/test_database.db");
-//        App.getApp().resetDb();
+        App.run();
+        App.getApp().setConnection("jdbc:sqlite:tests/test_database.db");
+        App.getApp().resetDb();
         this.supplierController = new SupplierController();
     }
 
@@ -30,7 +36,11 @@ class SupplierControllerTest {
     }
 
     @Test
-    void testIfGetAllSuppliersTakesParameters() {
+    void testIfGetAllSuppliersTakesParametersFromSQLITE() throws SQLException {
+        spark.Request req = mock(spark.Request.class);
+        Response res = mock(spark.Response.class);
+        HashMap testMap = (HashMap) SupplierController.getAllSuppliers(req, res).getModel();
+        assertTrue(testMap.size() > 0);
 
     }
 
